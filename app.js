79ada585390fed4d15db9760d360c96aeea09fc2
Chiwen.L <zxcvbnm840508@gmail.com>
2022-06-14 19:12:44 -0700

    add GET request

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 //imports
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //bodyparser
 app.use(express.json());
+
+//GET
+app.get('/', (req, res) => {
+  res.send('hi');
+});
 
 //listen
 app.listen(port, (req, res) =>
