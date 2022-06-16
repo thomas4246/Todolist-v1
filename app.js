d106cedd486ced3cae5beeb7385d476276c79109
Chiwen.L <zxcvbnm840508@gmail.com>
2022-06-15 23:29:03 -0700

    add switch for day

--- app.js
+++ app.js
@@ -13,7 +13,36 @@ app.set('view engine', 'ejs');
 
 //GET
 app.get('/', (req, res) => {
-  res.send('hi');
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = '';
+
+  switch (currentDay) {
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+  }
+
+  res.render('index', { day: day });
 });
 
 //listen
