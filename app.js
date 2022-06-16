f948fb284459f2034e25bdb9ad78daf422b2fcae
Chiwen.L <zxcvbnm840508@gmail.com>
2022-06-15 23:50:01 -0700

    replace switch with toLocaleDateString

--- app.js
+++ app.js
@@ -14,33 +14,14 @@ app.set('view engine', 'ejs');
 //GET
 app.get('/', (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = '';
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
-    default:
-  }
+  let option = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+
+  let day = today.toLocaleDateString('en-US', option);
 
   res.render('index', { day: day });
 });
