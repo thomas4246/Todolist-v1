3b0f0ab1cdc1e7606c38daecdd7a53c83d6facdb
Chiwen.L <zxcvbnm840508@gmail.com>
2022-06-18 18:16:32 -0700

    add post request for adding new items to the list

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+let newItems = [];
+
 //bodyparser
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //EJS
 app.set('view engine', 'ejs');
@@ -23,7 +25,15 @@ app.get('/', (req, res) => {
 
   let day = today.toLocaleDateString('en-US', option);
 
-  res.render('index', { day: day });
+  res.render('index', { day: day, newTodo: newItems });
+});
+
+app.post('/', (req, res) => {
+  let newItem = req.body.todo;
+
+  newItems.push(newItem);
+
+  res.redirect('/');
 });
 
 //listen
