89294d964b0ce9d5f16d2193469db8193411e970
Chiwen.L <zxcvbnm840508@gmail.com>
2022-06-26 14:31:48 -0700

    add default items into todolistDB

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 //imports
 const express = require('express');
 const date = require(__dirname + '/date.js');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const newItems = [];
+
+//setup DB connection
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/todolistDB');
+}
+
+//Item schema
+const itemsSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter a name for the item.'],
+  },
+});
+
+//Items collection
+const item = mongoose.model('item', itemsSchema);
+
+//Create some todo for the Items collection
+const todo1 = new item({
+  name: 'Welcome to your todolist!',
+});
+const todo2 = new item({
+  name: 'Hit the + button to add a new item',
+});
+const todo3 = new item({
+  name: '<-- Hit this to delete an item',
+});
+
+//Insert defult items into Items collection
+const defultItems = [todo1, todo2, todo3];
+
+item.insertMany(defultItems, (err) =>
+  err
+    ? console.log(err)
+    : console.log('Successfully added the items to the Items collection')
+);
 
 //Set static folder
 app.use(express.static('public'));
